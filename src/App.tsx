0ee241975367452e6fca955f5917f9ae183283f6
emretantu <emretantu@gmail.com>
2026-01-13 22:58:26 +0300

    Refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
-import styles from './App.modules.scss'
+// import styles from './App.modules.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <h1>Hello</h1>
